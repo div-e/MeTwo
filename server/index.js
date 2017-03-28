eae400d40a3e81fb91dc24027c616056da848bf3
Sui Gao <sugao@ucsd.edu>
2017-03-27 19:40:55 -0700

    improve server error handling

--- server/index.js
+++ server/index.js
@@ -4,24 +4,41 @@ const net = require('net')
 let robot = null
 
 const tcpServer = net.createServer(c => {
-    console.log('client connected')
     robot = c
-    c.on('close', err => {
+    console.log('robot connected')
+    c.on('close', had_error => {
         robot = null
-        console.log('client disconnected')
+        console.log(`socket is destroyed: ${c.destroyed}`)
+        if (had_error) {
+            console.log('robot disconnected due to an error')
+        }
+        else {
+            console.log('robot disconnected')
+        }
     })
 })
 tcpServer.listen(6000)
 
-const wss = new WebSocket.Server({
+const wsServer = new WebSocket.Server({
     perMessageDeflate: false,
     port: 6001
 })
 
-wss.on('connection', ws => {
+wsServer.on('connection', ws => {
+    console.log('browser connected')
     ws.on('message', message => {
         if (robot != null) {
-            robot.write(message)
+            try {
+                robot.write(message)
+            }
+            catch (e) {
+                robot = null
+                console.log('falied to communicate with the robot')
+                console.log(e)
+            }
         }
+    })
+    ws.on('close', () => {
+        console.log('browser disconnected')
     })
 })
