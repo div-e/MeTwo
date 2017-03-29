5f86fda08529688a91f12a68ab18add33c3b1cd2
D1o0g9s <boodalina@gmail.com>
2017-03-28 17:04:01 -0700

    making servo pwm work

--- client/controls.c
+++ client/controls.c
@@ -77,6 +77,10 @@ int init()
     pinMode(motorB_01, OUTPUT);
     pinMode(motorB_02, OUTPUT);
     pinMode(servo, PWM_OUTPUT);
+    
+    pwmSetMode(PWM_MODE_MS); 
+    pwmSetClock(384); //clock at 50kHz (20us tick)
+    pwmSetRange(1000); //range at 1000 ticks (20ms)
 
     pinMode(stdby, OUTPUT); 
     digitalWrite(stdby, HIGH); //Pull Standby high 
