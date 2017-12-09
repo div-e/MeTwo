6cb35d9688c852bd797637ecb1b9eec00b5f410d
D1o0g9s <boodalina@gmail.com>
2017-12-09 14:23:03 -0800

    added enum

--- server/pages/script.js
+++ server/pages/script.js
@@ -1,26 +1,46 @@
 ﻿'use strict';
 
+// Browser Signal Enum
+export var browserSignals = {
+        W_KEY_UP: 0, 
+        W_KEY_DOWN: 1,
+        A_KEY_UP: 2,
+        A_KEY_DOWN: 3,
+        S_KEY_UP: 4, 
+        S_KEY_DOWN: 5, 
+        D_KEY_UP: 6,
+        D_KEY_DOWN: 7, 
+        I_KEY_UP: 8,
+        I_KEY_DOWN: 9, 
+        K_KEY_UP: 10, 
+        K_KEY_DOWN: 11,
+        J_KEY_UP: 12,
+        J_KEY_DOWN: 13,
+        L_KEY_UP: 14,
+        L_KEY_DOWN: 15
+}
+
 (() => {
     let ws = null
     const keyDownSignals = new Map()
     const keyUpSignals = new Map()
     const img = document.getElementById('img')
-    keyDownSignals.set('w', new Uint8Array([1]).buffer)
-    keyDownSignals.set('s', new Uint8Array([2]).buffer)
-    keyUpSignals.set('w', new Uint8Array([3]).buffer)
-    keyUpSignals.set('s', new Uint8Array([4]).buffer)
-    keyDownSignals.set('a', new Uint8Array([5]).buffer)
-    keyDownSignals.set('d', new Uint8Array([6]).buffer)
-    keyUpSignals.set('a', new Uint8Array([7]).buffer)
-    keyUpSignals.set('d', new Uint8Array([8]).buffer)
-    keyDownSignals.set('i', new Uint8Array([9]).buffer)
-    keyDownSignals.set('k', new Uint8Array([10]).buffer)
-    keyUpSignals.set('i', new Uint8Array([11]).buffer)
-    keyUpSignals.set('k', new Uint8Array([12]).buffer)
-    keyDownSignals.set('j', new Uint8Array([13]).buffer)
-    keyDownSignals.set('l', new Uint8Array([14]).buffer)
-    keyUpSignals.set('j', new Uint8Array([15]).buffer)
-    keyUpSignals.set('l', new Uint8Array([16]).buffer)
+    keyDownSignals.set('w', new Uint8Array([browserSignals.W_KEY_DOWN]).buffer)
+    keyDownSignals.set('s', new Uint8Array([browserSignals.S_KEY_DOWN).buffer)
+    keyUpSignals.set('w', new Uint8Array([browserSignals.W_KEY_UP]).buffer)
+    keyUpSignals.set('s', new Uint8Array([browserSignals.S_KEY_UP]).buffer)
+    keyDownSignals.set('a', new Uint8Array([browserSignals.A_KEY_DOWN]).buffer)
+    keyDownSignals.set('d', new Uint8Array([browserSignals.D_KEY_DOWN]).buffer)
+    keyUpSignals.set('a', new Uint8Array([browserSignals.A_KEY_UP]).buffer)
+    keyUpSignals.set('d', new Uint8Array([browserSignals.D_KEY_UP]).buffer)
+    keyDownSignals.set('i', new Uint8Array([browserSignals.I_KEY_DOWN]).buffer)
+    keyDownSignals.set('k', new Uint8Array([browserSignals.K_KEY_DOWN]).buffer)
+    keyUpSignals.set('i', new Uint8Array([browserSignals.I_KEY_UP]).buffer)
+    keyUpSignals.set('k', new Uint8Array([browserSignals.K_KEY_UP]).buffer)
+    keyDownSignals.set('j', new Uint8Array([browserSignals.J_KEY_DOWN]).buffer)
+    keyDownSignals.set('l', new Uint8Array([browserSignals.L_KEY_DOWN]).buffer)
+    keyUpSignals.set('j', new Uint8Array([browserSignals.J_KEY_UP]).buffer)
+    keyUpSignals.set('l', new Uint8Array([browserSignals.L_KEY_UP]).buffer)
 
 
     connect()
