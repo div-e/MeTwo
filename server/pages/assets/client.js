ff6e530a4ab3f857abcc35eaf910d368c53db55c
thomastangerine <thomastang.thomas97@gmail.com>
2018-05-24 18:46:08 -0700

    Complete sending wasd keys to server, send in json format

--- server/pages/assets/client.js
+++ server/pages/assets/client.js
@@ -1,7 +1,6 @@
 const myPort = 8080;
 var counter = 0;
 
-
 var connection = new WebSocket('ws://localhost:' + myPort);
 
 connection.onopen = function() {
@@ -48,14 +47,85 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
   });
 
   joystick.addEventListener('touchStart', function(){
-    console.log('down')
+    console.log('down');
   });
 
   joystick.addEventListener('touchEnd', function(){
-    console.log('up')
+    console.log('up');
   });
 
+  /* send key signals */
+  var w = false;
+  var a = false;
+  var s = false;
+  var d = false;
+
+  /* set wasd booleans to true on key down */
+  document.onkeydown = function (event) {
+    if (event.keyCode == 87) {
+      w = true;
+    }
+    if (event.keyCode == 65) {
+      a = true;
+    }
+    if (event.keyCode == 83) {
+      s = true;
+    }
+    if (event.keyCode == 68) {
+      d = true;
+    }
+  }
+
+  /* set wasd booleans to false on key up */
+  document.onkeyup = function (event) {
+    if (event.keyCode == 87) {
+      w = false;
+    }
+    if (event.keyCode == 65) {
+      a = false;
+    }
+    if (event.keyCode == 83) {
+      s = false;
+    }
+    if (event.keyCode == 68) {
+      d = false;
+    }
+  }
   setInterval(function(){
-    connection.send(' dx:' + joystick.deltaX() + ' dy:' + joystick.deltaY());
+
+    /* calculate speed of left and right motors based on wasd keys */
+    var leftMotor = 0;
+    var rightMotor = 0;
+
+    if (w == true) {
+      leftMotor = leftMotor + 1;
+      rightMotor += 1;
+    }
+
+    if (a) {
+      leftMotor -= .5;
+      rightMotor += .5;
+    }
+
+    if (s) {
+      leftMotor -= 1;
+      rightMotor -= 1;
+    }
+
+    if (d) {
+      leftMotor += .5;
+      rightMotor -= .5;
+    }
+
+    /* convert to json string */
+    var jsonJoystick = {dx:joystick.deltaX(), dy:joystick.deltaY()};
+    var jsonJoystickString = JSON.stringify(jsonJoystick);
+
+    var jsonWASD = {leftMotor: leftMotor, rightMotor: rightMotor};
+    var jsonWASDString = JSON.stringify(jsonWASD);
+
+    /* send json strings */
+    connection.send(jsonJoystickString);
+    connection.send(jsonWASDString);
   }, 1/30 * 1000);
 }
