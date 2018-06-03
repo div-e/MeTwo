22b173c3b4eb90900019676b03d1b204c4bd0831
kevinyoussef <keviny990@gmail.com>
2018-06-03 16:51:03 -0700

    Changed to fit new serial input. Cleaned unnecessary comments

--- arduino/Servo-joystick_test1/Servo-joystick_test1.ino
+++ arduino/Servo-joystick_test1/Servo-joystick_test1.ino
@@ -1,38 +1,51 @@
-#include <Servo.h>
-Servo xservo, yservo;
-int dx,dy,xpos=90,ypos=90;
+  #include <Servo.h>
+  #include <Adafruit_PWMServoDriver.h>
+  #include <Wire.h>
+  #define SERVOMIN  125 // this is the 'minimum' pulse length count (out of 4096)
+  #define SERVOMAX  575 // this is the 'maximum' pulse length count (out of 4096)
+
+  Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();  
+  Servo xservo, yservo;
+  int dx,dy,xpos=90,ypos=90;
 void setup() {
   Serial.begin(9600);
   Serial.setTimeout(50);
   xservo.attach(9);
   yservo.attach(11);
+  
+  xservo.write(xpos);   //Initialize camera to default position
+  yservo.write(ypos);
+  
+  pwm.begin();
+  pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
+  yield();  
+  
+  pwm.setPWM(0, 0, angleToPulse(xpos) );    //Initialize Camera to default position
+  pwm.setPWM(1, 0, angleToPulse(ypos) );
+  
 }
 
 void loop() {
-String STR="";
-char x[45];
+  String STR="";
+  char x[45];
   Serial.println("dx=");
   while(!Serial.available());
   STR=Serial.readString(); //read the sting in the buffer and store it into DX
 
-  STR.toCharArray(x, 45);  //convert string to char array
+  STR.toCharArray(x, 6);  //convert string to char array
   
-  Serial.println(x[13]);  //print out the integers
-  Serial.println(x[28]);
-  Serial.println(x[35]);
-  Serial.println(x[43]);
-  Serial.println("Values");
-  int RM=x[13]-48; //Convert ASCII to Integers
-  int LF=x[28]-48;
-  int dx=x[35]-48;
-  int dy=x[42]-48;
+  int RM=x[0]-48; //Convert ASCII to Integers
+  int LF=x[1]-48;
+  int dx=x[2]-48;
+  int dy=x[3]-48;
   Serial.println(RM);     //print the values of the integers after they are stored
   Serial.println(LF);
   Serial.println(dx);
   Serial.println(dy);
   //Control the Camera
-  dx=dx-5;    //take away the offset
-  dy=dy-5;
+  dx=10*(dx-5);    //take away the offset and multiply
+  dy=10*(dy-5);
+  
   if(dx>0)
   {
     xpos=min(180,xpos+dx); //move to the right
@@ -55,42 +68,13 @@ char x[45];
   Serial.println(ypos);
   xservo.write(xpos);
   yservo.write(ypos);
-      
+  pwm.setPWM(0, 0, angleToPulse(xpos) );
+  pwm.setPWM(1, 0, angleToPulse(ypos) );
   
 }
- /* Char=Serial.read();
-      if (isDigit(Char)) {
-      // convert the incoming byte to a char and add it to the string:
-      DX += (char)Char;
-      dx=DX.toInt();
-  Serial.println(dx);
-  Serial.println("dy=");
-  while(!Serial.available());
-  Char=Serial.read();
-      if (isDigit(Char)) {
-      // convert the incoming byte to a char and add it to the string:
-      DY += (char)Char;
-  dy=DY.toInt();
-  Serial.println(dy);
-  if(dx>0)
-  {
-    pos=min(180,pos+dx); //move to the right
-  }
-  if(dx<0)
-  {
-    pos=max(15,pos+dx);  //move to the left
-  }
-  if(dy>0)
-  {
-    pos2=max(15,pos2-dy); //move down
-  }
-  if(dy<0)
-  {
-    pos2=min(180,pos2-dy); //move up
-  }
-  Serial.print("pos=");
-  Serial.println(pos);
-  Serial.print("pos2=");
-  Serial.println(pos2);
-      }}}
-*/
+
+int angleToPulse(int ang){
+   int pulse = map(ang,0, 180, SERVOMIN,SERVOMAX);// map angle of 0 to 180 to Servo min and Servo max 
+  
+   return pulse;
+}
