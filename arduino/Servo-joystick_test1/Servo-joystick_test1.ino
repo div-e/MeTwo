25abe6612a27f3a577436f2e4d87e96beca64e2b
gypang <gypang24@gmail.com>
2018-06-03 19:37:09 -0700

    finalized buggy controls code

--- arduino/Servo-joystick_test1/Servo-joystick_test1.ino
+++ arduino/Servo-joystick_test1/Servo-joystick_test1.ino
@@ -1,80 +1,80 @@
-  #include <Servo.h>
-  #include <Adafruit_PWMServoDriver.h>
-  #include <Wire.h>
-  #define SERVOMIN  125 // this is the 'minimum' pulse length count (out of 4096)
-  #define SERVOMAX  575 // this is the 'maximum' pulse length count (out of 4096)
+#include <Servo.h>
+#include <Adafruit_PWMServoDriver.h>
+#include <Wire.h>
+#define SERVOMIN  125 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  575 // this is the 'maximum' pulse length count (out of 4096)
 
-  Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();  
-  Servo xservo, yservo;
-  int dx,dy,xpos=90,ypos=90;
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+Servo xservo, yservo;
+int dx,dy,xpos=90,ypos=90;
 void setup() {
-  Serial.begin(9600);
-  Serial.setTimeout(50);
-  xservo.attach(9);
-  yservo.attach(11);
-  
-  xservo.write(xpos);   //Initialize camera to default position
-  yservo.write(ypos);
-  
-  pwm.begin();
-  pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
-  yield();  
-  
-  pwm.setPWM(0, 0, angleToPulse(xpos) );    //Initialize Camera to default position
-  pwm.setPWM(1, 0, angleToPulse(ypos) );
-  
+    Serial.begin(9600);
+    Serial.setTimeout(50);
+    xservo.attach(9);
+    yservo.attach(11);
+
+    xservo.write(xpos);   //Initialize camera to default position
+    yservo.write(ypos);
+
+    pwm.begin();
+    pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
+    yield();
+
+    pwm.setPWM(0, 0, angleToPulse(xpos) );    //Initialize Camera to default position
+    pwm.setPWM(1, 0, angleToPulse(ypos) );
+
 }
 
 void loop() {
-  String STR="";
-  char x[45];
-  Serial.println("dx=");
-  while(!Serial.available());
-  STR=Serial.readString(); //read the sting in the buffer and store it into DX
+    String STR="";
+    char x[45];
+    Serial.println("dx=");
+    while(!Serial.available());
+    STR=Serial.readString(); //read the sting in the buffer and store it into DX
 
-  STR.toCharArray(x, 6);  //convert string to char array
-  
-  int RM=x[0]-48; //Convert ASCII to Integers
-  int LF=x[1]-48;
-  int dx=x[2]-48;
-  int dy=x[3]-48;
-  Serial.println(RM);     //print the values of the integers after they are stored
-  Serial.println(LF);
-  Serial.println(dx);
-  Serial.println(dy);
-  //Control the Camera
-  dx=10*(dx-5);    //take away the offset and multiply
-  dy=10*(dy-5);
-  
-  if(dx>0)
-  {
-    xpos=min(180,xpos+dx); //move to the right
-  }
-  if(dx<0)
-  {
-    xpos=max(15,xpos+dx);  //move to the left
-  }
-  if(dy>0)
-  {
-    ypos=max(15,ypos-dy); //move down
-  }
-  if(dy<0)
-  {
-    ypos=min(180,ypos-dy); //move up
-  }
-  Serial.print("Xpos=");
-  Serial.println(xpos);
-  Serial.print("Ypos=");
-  Serial.println(ypos);
-  xservo.write(xpos);
-  yservo.write(ypos);
-  pwm.setPWM(0, 0, angleToPulse(xpos) );
-  pwm.setPWM(1, 0, angleToPulse(ypos) );
-  
+    STR.toCharArray(x, 6);  //convert string to char array
+
+    int RM=x[0]-48; //Convert ASCII to Integers
+    int LF=x[1]-48;
+    int dx=x[2]-48;
+    int dy=x[3]-48;
+    Serial.println(RM);     //print the values of the integers after they are stored
+    Serial.println(LF);
+    Serial.println(dx);
+    Serial.println(dy);
+    //Control the Camera
+    dx=10*(dx-5);    //take away the offset and multiply
+    dy=10*(dy-5);
+
+    if(dx>0)
+    {
+        xpos=min(180,xpos+dx); //move to the right
+    }
+    if(dx<0)
+    {
+        xpos=max(15,xpos+dx);  //move to the left
+    }
+    if(dy>0)
+    {
+        ypos=max(15,ypos-dy); //move down
+    }
+    if(dy<0)
+    {
+        ypos=min(180,ypos-dy); //move up
+    }
+    Serial.print("Xpos=");
+    Serial.println(xpos);
+    Serial.print("Ypos=");
+    Serial.println(ypos);
+    xservo.write(xpos);
+    yservo.write(ypos);
+    pwm.setPWM(0, 0, angleToPulse(xpos) );
+    pwm.setPWM(1, 0, angleToPulse(ypos) );
+
 }
 
 int angleToPulse(int ang){
-   int pulse = map(ang,0, 180, SERVOMIN,SERVOMAX);// map angle of 0 to 180 to Servo min and Servo max 
-  
-   return pulse;
+    int pulse = map(ang,0, 180, SERVOMIN,SERVOMAX);// map angle of 0 to 180 to Servo min and Servo max
+
+    return pulse;
 }
--- arduino/motorEncoder/motorEncoder.ino
+++ arduino/motorEncoder/motorEncoder.ino
@@ -1,3 +1,10 @@
+#include <Servo.h>
+#include <Adafruit_PWMServoDriver.h>
+#include <Wire.h>
+
+#define SERVOMIN  125 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  575 // this is the 'maximum' pulse length count (out of 4096)
+
 #define leftMotorOutputA 4
 #define leftMotorOutputB 5
 #define rightMotorOutputA 6
@@ -9,7 +16,7 @@
 #define leftEncoderInputA 3
 #define leftEncoderInputB 2 // 2 for interrupt
 #define rightEncoderInputA 3 // TODO CHANGE THIS
-#define rightEncoderInputA 2 // TODO CHANGE THIS
+#define rightEncoderInputB 2 // TODO CHANGE THIS
 
 #define leftArmMotorOutputA 8
 #define leftArmMotorOutputB 9
@@ -29,12 +36,24 @@
 #define motorSpeedLevel2 160
 #define motorSpeedLevel3 240
 
+//#define xServoPin 9
+//#define yServoPin 11
+
+#define defaultServoPosition 90
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+int dx, dy, xServoPosition, yServoPosition;
+
+// keeps track of whether or not arm is deployed
+/* bool leftArmDeployed = false; */
+/* bool rightArmDeployed = false; */
+bool armDeployed = false;
+
 volatile long leftEncoderValue; // keeps track of left arm encoder value
 volatile long rightEncoderValue; // keeps track of right arm encoder value
 
-int prevLeftMotor = -1;
-int prevRightMotor = -1;
-int prevDeploy = -1;
+Servo xServo, yServo; // Servo vars
+int inputIntArr[5];
 
 void setup() {
 
@@ -60,13 +79,70 @@ void setup() {
     attachInterrupt(digitalPinToInterrupt(leftEncoderInputB), leftEncoderEvent, CHANGE);
     attachInterrupt(digitalPinToInterrupt(rightEncoderInputB), rightEncoderEvent, CHANGE);
 
+    /* xServo.attach(xServoPin); */
+    /* yServo.attach(yServoPin); */
+
+    /* xServo.write(defaultServoPosition); */
+    /* yServo.write(defaultServoPosition); */
+
+    pwm.begin();
+    pwm.setPWMFreq(60);
+    yield();
+
+    // Initialize camera to default position
+    setServoPWM(defaultServoPosition, defaultServoPosition);
+
     Serial.begin (9600);
+    Serial.setTimeout(50);
+    while(!Serial.available());
 }
 
 void loop() {
     // infinite loop that pauses loop until new serial msg is available
-    while(!Serial.available());
 
+    Serial.print("\nxServoPosition: ");
+    Serial.println(xServoPosition);
+    Serial.print("\nyServoPosition: ");
+    Serial.println(yServoPosition);
+
+    for (int i = 0 ; i < 5 ; i++){
+      inputIntArr[i] = 0;
+    }
+    convertSerialStringToIntArray();
+
+    setLeftMotor(inputIntArr[0]);
+    setRightMotor(inputIntArr[1]);
+
+    setXServo(inputIntArr[2]);
+    setYServo(inputIntArr[3]);
+    setServoPWM(xServoPosition, yServoPosition);
+
+    setArmsState(inputIntArr[4]);
+    stopArmThresholdCheck();
+}
+
+void convertSerialStringToIntArray(){
+    char tempCharArr[5];
+
+    if(Serial.available()){
+      Serial.readString().toCharArray(tempCharArr, 6);
+    }
+
+    // Convert ASCII to ints
+    for (int i = 0 ; i < 5 ; i++){
+        inputIntArr[i] = tempCharArr[i] - 48;
+        
+        Serial.print("\ninputArrInt[");
+        Serial.print(i);
+        Serial.print("]: ");
+        Serial.println(inputIntArr[i]);
+
+        Serial.print("\ntempCharArr[");
+        Serial.print(i);
+        Serial.print("]: ");
+        Serial.println(tempCharArr[i]);
+        
+    }
 }
 
 int mapMotorSpeedToPWM(int motorSpeed){
@@ -109,6 +185,18 @@ void setRightMotor(int motorSpeed){
     }
 }
 
+void setArmsState(int armState){
+    bool boolArmState = (armState == 1);
+
+    if(boolArmState != armDeployed){
+        if(boolArmState){
+            retractArms();
+        } else {
+            deployArms();
+        }
+    }
+}
+
 void deployArms(){
     analogWrite(leftArmSpeed, armSpeed);
     analogWrite(rightArmSpeed, armSpeed);
@@ -147,14 +235,24 @@ void stopArmThresholdCheck(){
 
     // above code snippet is faster version of digitalWrite.
 
-    if (leftEncodeValue < 0 || leftEncodeValue > armThreshold){
+    if (leftEncoderValue < 0 || leftEncoderValue > armThreshold){
         digitalWrite(leftArmMotorOutputA, LOW);
         digitalWrite(leftArmMotorOutputB, LOW);
+        if (leftEncoderValue < 0){
+            armDeployed = false;
+        } else {
+            armDeployed = true;
+        }
     }
 
-    if (rightEncodeValue < 0 || rightEncodeValue > armThreshold){
+    if (rightEncoderValue < 0 || rightEncoderValue > armThreshold){
         digitalWrite(rightArmMotorOutputA, LOW);
         digitalWrite(rightArmMotorOutputB, LOW);
+        if (rightEncoderValue < 0){
+            armDeployed = false;
+        } else {
+            armDeployed = true;
+        }
     }
 }
 
@@ -196,4 +294,44 @@ void rightEncoderEvent(){
             rightEncoderValue++;
         }
     }
+}
+
+int mapServoStateToPosition(int state){
+    return 10*(state-5);
+}
+
+void setXServo(int dx){
+    dx = mapServoStateToPosition(dx);
+    if (dx > 0){
+        // move to right
+        xServoPosition = min(180, xServoPosition+dx);
+    } else if (dx < 0){
+        // move to left
+        xServoPosition = max(15, xServoPosition+dx);
+    }
+}
+
+void setYServo(int dy){
+    dy = mapServoStateToPosition(dy);
+    if (dy > 0){
+        // move down
+        yServoPosition = min(180, yServoPosition+dy);
+    } else if (dx < 0){
+        // move up
+        yServoPosition = max(15, yServoPosition+dy);
+    }
+}
+
+
+void setServoPWM(int xServoPosition, int yServoPosition){
+    pwm.setPWM(0, 0, angleToPulse(xServoPosition));
+    pwm.setPWM(0, 0, angleToPulse(yServoPosition));
+}
+
+int angleToPulse(int angle){
+    int pulse = map(angle, 0, 180, SERVOMIN, SERVOMAX); // map angle of 0 to 180
+                                                        // to Servo min and
+                                                        // Servo max
+
+    return pulse;
 }
